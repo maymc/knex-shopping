de150b718e38e08283bb602b639b9419a7c5f29d
May Chen <maychen@Mays-MacBook-Pro.local>
2018-09-25 22:40:55 -1000

    GET user by id works

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Router = express.Router();
+knex = require('../knex/knex.js');
 
 Router.get('/', (req, res) => {
   console.log("Users - Home route");
   res.json("Welcome to Users");
+});
+
+Router.get('/users/:user_id', (req, res) => {
+  console.log("req.params:", req.params);
+  const { id } = req.params;
+  console.log("id:", { id });
+  knex.raw(`SELECT * FROM users WHERE id = ${id}`)
+    .then(results => {
+      console.log("user results:", results.row[0]);
+    })
+    .catch(err => {
+      console.log('User not found', err);
+    })
 });
 
 module.exports = Router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+this.knex = require('./knex/knex.js');
 
 const usersRoutes = require('./routes/users');
 const productsRoutes = require('./routes/products');
@@ -12,16 +13,33 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 ////////////////////////////////////
 
-app.use('/users', usersRoutes);
-app.use('/products', productsRoutes);
-app.use('/cart', cartRoutes);
+// app.use('/users', usersRoutes);
+// app.use('/products', productsRoutes);
+// app.use('/cart', cartRoutes);
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   console.log("User attempted to get a URI.");
-  res.json("404");
+  res.json("This is the homepage.");
 });
 
+app.get('/users', (req, res) => {
+  console.log("Users - Home route");
+  res.json("Welcome to Users here");
+});
 
+app.get('/users/:user_id', (req, res) => {
+  console.log("req.params:", req.params);
+  const { user_id } = req.params;
+  console.log("id:", user_id);
+  this.knex.raw(`SELECT * FROM users WHERE id = ${user_id}`)
+    .then(results => {
+      console.log("user results:", results.rows[0]);
+      res.json(results.rows[0]);
+    })
+    .catch(err => {
+      console.log('User not found', err);
+    })
+});
 
 
 
